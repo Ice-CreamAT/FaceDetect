cf58b15c4beb641389cf91fc1d087e3aca9da38c
AT <73790661+Ice-CreamAT@users.noreply.github.com>
2023-11-28 22:00:44 -0300

    Update Face_Detection.py
    Commit edit

--- Face_Detection.py
+++ Face_Detection.py
@@ -11,18 +11,25 @@ detections = classifier.detectMultiScale(grey_image, scaleFactor=1.1,
                                            minSize=(30,30),
                                            maxSize=(1000,1000))
 
+#------------------------------------------------------------------------
 # ScaleFactor: resize a larger object to a smaller one. Slower if the value is smaller.
 # minNeighbors: How many neighbors must each candidate rectangle have to keep it. Higher values = higher quality.
 # minSize: Specifies the smallest object to be recognized.
 # maxSize: (I think it's already obvious.)
+#------------------------------------------------------------------------
 
-print(detections)             # Show the positions of each face in the image.
-print(len(detections))        # Show the number of detected faces.
+
+print(detections)        # Show the positions of each face in the image.
+print(len(detections))   # Show the number of detected faces.
+
 
  # Each face has an "x" and "y" coordinate.
  # "w" and "h" are for width and height.
+
+
 for (x, y, w, h) in detections:
      # Parameters for drawing the detection rectangle.
+  
     cv2.rectangle(image, (x, y), (x + w, y + h), (0,255,0), 2)
 
 cv2.imshow('Face Detection', image)      # Show image with title.
